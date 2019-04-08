b85528511cadb8f119435f0a2c1d674ab8c947f3
Daniel Bovington <d.bovington@gmail.com>
2019-04-08 09:48:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,9 +32,7 @@ def monopoly_with_third_tier
   	  :railroads => {
   	    :pieces => 4,
   	    :names => {
-  	      :reading_railroad => {
-  	        mortgage_value: "$100"
-  	      },
+  	      :reading_railroad => {},
   	      :pennsylvania_railroad => {},
   	      :b_and_o_railroad => {},
   	      :shortline =>{}
@@ -55,7 +53,9 @@ def monopoly_with_fourth_tier
     	  :railroads => {
     	    :pieces => 4,
     	    :names => {
-    	      :reading_railroad => {},
+    	      :reading_railroad => {
+    	         mortgage_value: "$100"
+    	      },
     	      :pennsylvania_railroad => {},
     	      :b_and_o_railroad => {},
     	      :shortline =>{}
